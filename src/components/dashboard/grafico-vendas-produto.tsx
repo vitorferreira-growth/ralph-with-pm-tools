223e7095351a7e12663b0978db9a76a251e6db07
Vitor Ferreira <vitor.ferreira@cloudwalk.io>
2026-01-15 09:27:29 -0300

    auto: Update src/components/dashboard/grafico-vendas-produto.tsx [09:27:29]

--- src/components/dashboard/grafico-vendas-produto.tsx
+++ src/components/dashboard/grafico-vendas-produto.tsx
@@ -106,28 +106,16 @@ interface ActiveShapeProps {
 }
 
 function renderActiveShape(props: ActiveShapeProps): React.ReactElement {
-  const RADIAN = Math.PI / 180
   const {
     cx,
     cy,
-    midAngle,
     innerRadius,
     outerRadius,
     startAngle,
     endAngle,
     fill,
     payload,
-    percent,
   } = props
-  const sin = Math.sin(-RADIAN * midAngle)
-  const cos = Math.cos(-RADIAN * midAngle)
-  const sx = cx + (outerRadius + 10) * cos
-  const sy = cy + (outerRadius + 10) * sin
-  const mx = cx + (outerRadius + 30) * cos
-  const my = cy + (outerRadius + 30) * sin
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22
-  const ey = my
-  const textAnchor = cos >= 0 ? 'start' : 'end'
 
   return (
     <g>
